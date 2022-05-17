7625976ac386ac63b0e1645e619ae51731cf87a1
keshab4855 <keshabroka123@gmail.com>
2022-05-18 03:20:04 +1000

    Edit and delete the data

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function insertNewRecord(data) {
   const table = document
     .getElementById("storeList")
     .getElementsByTagName("tbody")[0];
-  const newRow = table.insertRow(table.length);
+  newRow = table.insertRow(table.length);
   cell1 = newRow.insertCell(0);
   cell1.innerHTML = data.productCode;
   cell2 = newRow.insertCell(1);
@@ -37,4 +37,28 @@ function insertNewRecord(data) {
   cell4.innerHTML = data.perPrice;
   cell4 = newRow.insertCell(4);
   cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
+}
+
+//Edit the data
+function onEdit(td) {
+  selectedRow = td.parentElement.parentElement;
+  document.getElementById("productCode").value = selectedRow.cells[0].innerHTML;
+  document.getElementById("product").value = selectedRow.cells[1].innerHTML;
+  document.getElementById("qty").value = selectedRow.cells[2].innerHTML;
+  document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+  selectedRow.cells[0].innerHTML = formData.productCode;
+  selectedRow.cells[1].innerHTML = formData.product;
+  selectedRow.cells[2].innerHTML = formData.qty;
+  selectedRow.cells[3].innerHTML = formData.perPrice;
+}
+
+//Delete the data
+function onDelete(td) {
+  if (confirm("Do you want to delete this record?")) {
+    row = td.parentElement.parentElement;
+    document.getElementById("storeList").deleteRow(row.rowIndex);
+    resetForm();
+  }
 }
